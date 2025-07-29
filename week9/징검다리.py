84bc8fbca7fa177fbcd300fe61c7416f749360f8
HyeonTae Jang <gusxo999@gmail.com>
2025-07-29 15:46:20 +0900

    week10

--- week9/징검다리.py
+++ week9/징검다리.py
@@ -25,3 +25,33 @@ def solution(distance, rocks, n):
 
 
 solution(16, [1, 5, 11, 14], 1)
+
+# def correct_solution(distance, rocks, n):
+#     rocks.sort()
+#     rocks = [0] + rocks + [distance]
+    
+#     def can_achieve_min_distance(min_dist):
+#         removed = 0
+#         current_pos = 0
+        
+#         for i in range(1, len(rocks)):
+#             if rocks[i] - rocks[current_pos] < min_dist:
+#                 removed += 1
+#                 if removed > n:
+#                     return False
+#             else:
+#                 current_pos = i
+#         return True
+    
+#     left, right = 1, distance
+#     answer = 0
+    
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if can_achieve_min_distance(mid):
+#             answer = mid
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+    
+#     return answer
